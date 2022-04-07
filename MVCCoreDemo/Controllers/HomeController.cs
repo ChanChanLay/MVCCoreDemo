cdfc0beababd214a854637726d2056784c8c9f1e
chanlay <ayechan.m.cnsa@gmail.com>
2022-04-07 16:24:24 +0630

    プロジェクト ファイルを追加します。

---
+++ MVCCoreDemo/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MVCCoreDemo.Models;
+using MySql.Data.MySqlClient;
+
+namespace MVCCoreDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration Configuration;
+
+        public HomeController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            List<Room> rooms = new List<Room>();
+            //connect to Mysql
+            string connString = this.Configuration.GetConnectionString("Default");
+            using (MySqlConnection con = new MySqlConnection(connString))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from room;", con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    Room room = new Room();
+                    room.id = Convert.ToInt32(reader["roomId"]);
+                    room.no = Convert.ToInt32(reader["roomNo"]);
+                    room.type = reader["roomType"].ToString();
+                    room.adult = Convert.ToInt32(reader["adult"]);
+                    room.child = Convert.ToInt32(reader["child"]);
+                    room.price = Convert.ToInt32(reader["price"]);
+
+                    rooms.Add(room);
+                }
+                reader.Close();
+                con.Close();    
+            }
+                return View(rooms);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCCoreDemo/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreDemo.Models
+{
+    public class Room
+    {
+        public int id { get; set; }
+        public int no { get; set; }
+        public string type { get; set; }
+        public int adult { get; set; }
+        public int child { get; set; }
+        public int price { get; set; }
+    }
+}
